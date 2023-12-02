207c814addf84ea42b8202e0b805bb0421209385
Ashraf Atef <ashraf.programer@gmail.com>
2023-12-02 15:03:45 +0200

    task

--- shell.c
+++ shell.c
@@ -1,5 +1,51 @@
+#include "main.h"
+#include <string.h>
+
+void get_input(char *buff, int *size)
+{
+	*size = read(0, buff, BUFSIZ);
+	if (*size == -1)
+	{
+		exit(2);
+	}
+	buff[*size - 1] = '\0';
+}
+char *get_path(char *envp[])
+{
+	char *path = "PATH=";
+	char cur_env[100];
+	char *paths;
+	int i = 0;
+
+	while (envp[i])
+	{
+		memcpy(cur_env, envp[i], strlen(path));
+		if (strcmp(path, cur_env) == 0)
+		{
+			break;
+		}
+		i++;
+	}
+	return (envp[i] + strlen(path));
+}
 int main(int argc, char *argv[], char *envp[])
 {
-	/* code */
-	return 0;
+	char *path = get_path(envp);
+
+	printf("%s\n", path);
+	while (1)
+	{
+		char str[BUFSIZ];
+		int str_Size;
+
+		get_input(str, &str_Size);
+		if (!strcmp(str, "exit") || str_Size == 0)
+		{
+			printf("bye!\n");
+			break;
+		}
+		printf("%s\n", str);
+	}
+
+	return (0);
 }
