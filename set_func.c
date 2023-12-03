2f38335913d89ffc889b2f97d900ff3ade73f15e
RamzyAR7 <rameya683@gmail.com>
2023-12-03 21:50:47 +0200

    add all

--- set_func.c
+++ set_func.c
@@ -89,3 +89,15 @@ int handle_unsetenv(char *envp[], char *argv[])
 		i++;
 	}
 }
+int handle_env(char *envp[])
+{
+	int i = 0;
+
+	while (envp[i])
+	{
+		write(1, envp[i], _strlen(envp[i]));
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
