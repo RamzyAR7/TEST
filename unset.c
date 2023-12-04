e561813e56474dc938e95ffeab5c5b113d2a05fd
RamzyAR7 <rameya683@gmail.com>
2023-12-04 20:39:55 +0200

    sss

---
+++ unset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+int handle_unsetenv(char *argv[], char *envp[])
+{
+	int i = 0, x = 0;
+
+	while (envp[i])
+	{
+		if (_strcmp(envp[i], argv[1]))
+		{
+			free(envp[i]);
+
+			x = i;
+
+			while (envp[x])
+			{
+				envp[x] = envp[x + 1];
+				x++;
+			}
+			return (0);
+		}
+		i++;
+		return (-1);
+	}
+}
