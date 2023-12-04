070c352876296a70c0222f801168f689521e6067
Ashraf Atef <ashraf.programer@gmail.com>
2023-12-04 23:51:57 +0200

    task

--- unset.c
+++ unset.c
@@ -3,7 +3,6 @@
 int handle_unsetenv(char *argv[], char *envp[])
 {
 	int i = 0, x = 0;
-	printf("before:\n");
 	while (envp[i])
 	{
 		if (_strstr(envp[i], argv[1]) == envp[i] && envp[i][_strlen(argv[1])] == '=')
