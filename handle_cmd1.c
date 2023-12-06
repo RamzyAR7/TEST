e8b9b9b4e1abc75c51bb340f30ba3a5ca3e35cd7
Ashraf Atef <ashraf.programer@gmail.com>
2023-12-06 13:12:22 +0200

    task

--- handle_cmd1.c
+++ handle_cmd1.c
@@ -158,7 +158,7 @@ int handle_error(char **envp, char *first_sigment, char *path)
 	handle_env(envp);
 	_strcpy(error, "./hsh: line 1: ");
 	if (!envp[0] || !*envp[0] || _strcmp(envp[0], "_=") == 0 ||
-		!_strchr(envp[0], '='))
+		!_strchr(envp[0], '=') || _strlen((char *)envp) < 4)
 	{
 		_strcpy(error, "./hsh: 1: ");
 		_strcpy(error + _strlen(error), first_sigment);
