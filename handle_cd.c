9af63a0708b1c4ecc9f57bdee0eccf37d336ee1f
Ashraf Atef <ashraf.programer@gmail.com>
2023-12-10 16:08:55 +0200

    task

--- handle_cd.c
+++ handle_cd.c
@@ -2,24 +2,24 @@
 
 /**
  * handle_cd - handles the cd command
- * @arugments: pointer to array of arguments
+ * @arguments: pointer to array of arguments
  * Return: 0 if successful, otherwise 1
  * This function is not portable. It will only work on Linux.
  * by ashraf & ramzy
  */
-int handle_cd(char **arugments)
+int handle_cd(char **arguments)
 {
 	char cwd[BUFFER_SIZE];
 	char *cd = NULL;
 	char **pwd, **oldpwd;
 
 	getcwd(cwd, sizeof(cwd));
-	pwd = creat_2D(3, "setenv", "PWD", arugments[1]);
+	pwd = creat_2D(3, "setenv", "PWD", arguments[1]);
 	oldpwd = creat_2D(3, "setenv", "OLDPWD", cwd);
 
-	if (arugments[1] && *arugments[1])
+	if (arguments[1] && *arguments[1])
 	{
-		if (!handle_cd_helper(arugments, pwd, oldpwd, cd, cwd))
+		if (!handle_cd_helper(arguments, pwd, oldpwd, cd, cwd))
 			return (0);
 	}
 	else
@@ -34,14 +34,23 @@ int handle_cd(char **arugments)
 	}
 	handle_setenv(pwd);
 	handle_setenv(oldpwd);
-	arguments_free(arugments);
+	arguments_free(arguments);
 	return (0);
 }
-
-int handle_cd_helper(char **arugments, char **pwd,
+/**
+ * handle_cd_helper - handle cd
+ * @arguments: pointer to array of arguments
+ * @pwd: pointer to array of pwd arguments
+ * @oldpwd: pointer to array of oldpwd arguments
+ * @cd: pointer of string should go to
+ * @cwd: pointer of string of current directory
+ * Return: 1 if successful, otherwise 0
+ * by ashraf & ramzy
+ */
+int handle_cd_helper(char **arguments, char **pwd,
 					 char **oldpwd, char *cd, char *cwd)
 {
-	if (_strcmp(arugments[1], "-") == 0)
+	if (_strcmp(arguments[1], "-") == 0)
 	{
 		cd = get_env_value("OLDPWD");
 		if (cd)
@@ -61,21 +70,21 @@ int handle_cd_helper(char **arugments, char **pwd,
 	}
 	else
 	{
-		if (chdir(arugments[1]) == -1)
+		if (chdir(arguments[1]) == -1)
 		{
 			if (errno == ENOENT)
 			{
 				char *error = "./hsh: 1: cd: can't cd to ";
 
-				print(STDERR_FILENO, error, arugments[1], "\n", NULL);
+				print(STDERR_FILENO, error, arguments[1], "\n", NULL);
 			}
 			else if (errno == EACCES)
 			{
 				char *error = "./hsh: 1: cd: can't cd to ";
 
-				print(STDERR_FILENO, error, arugments[1], "\n", NULL);
+				print(STDERR_FILENO, error, arguments[1], "\n", NULL);
 			}
-			arguments_free(pwd), arguments_free(oldpwd), arguments_free(arugments);
+			arguments_free(pwd), arguments_free(oldpwd), arguments_free(arguments);
 			return (0);
 		}
 	}
