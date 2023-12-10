e5132161eb29998ada6bd017b8345a6490902fac
Ashraf Atef <ashraf.programer@gmail.com>
2023-12-10 15:01:18 +0200

    task

--- handle_cd.c
+++ handle_cd.c
@@ -12,52 +12,14 @@ int handle_cd(char **arugments)
 	char cwd[BUFFER_SIZE];
 	char *cd = NULL;
 	char **pwd, **oldpwd;
+
 	getcwd(cwd, sizeof(cwd));
 	pwd = creat_2D(3, "setenv", "PWD", arugments[1]);
 	oldpwd = creat_2D(3, "setenv", "OLDPWD", cwd);
 
 	if (arugments[1] && *arugments[1])
 	{
-		if (_strcmp(arugments[1], "-") == 0)
-		{
-			cd = get_env_value("OLDPWD");
-			if (cd)
-			{
-				_Free(pwd[2]);
-				pwd[2] = cd;
-				chdir(cd);
-				getcwd(cwd, sizeof(cwd));
-				print(STDOUT_FILENO, cwd, "\n", NULL);
-			}
-			else
-			{
-				print(STDOUT_FILENO, cwd, "\n", NULL);
-				_Free(oldpwd[2]);
-				_Free(pwd[2]);
-			}
-		}
-		else
-		{
-			if (chdir(arugments[1]) == -1)
-			{
-				if (errno == ENOENT)
-				{
-					char *error = "./hsh: 1: cd: can't cd to ";
-
-					print(STDERR_FILENO, error, arugments[1], "\n", NULL);
-				}
-				else if (errno == EACCES)
-				{
-					char *error = ": Permission denied\n";
-
-					print(STDERR_FILENO, "./hsh: line 1: cd: ", arugments[1], error, NULL);
-				}
-				arguments_free(pwd);
-				arguments_free(oldpwd);
-				arguments_free(arugments);
-				return (0);
-			}
-		}
+		handle_cd_helper(arugments, pwd, oldpwd, cd, cwd);
 	}
 	else
 	{
@@ -73,4 +35,49 @@ int handle_cd(char **arugments)
 	handle_setenv(oldpwd);
 	arguments_free(arugments);
 	return (0);
+}
+
+void handle_cd_helper(char **arugments, char **pwd,
+					  char **oldpwd, char *cd, char *cwd)
+{
+	if (_strcmp(arugments[1], "-") == 0)
+	{
+		cd = get_env_value("OLDPWD");
+		if (cd)
+		{
+			_Free(pwd[2]);
+			pwd[2] = cd;
+			chdir(cd);
+			getcwd(cwd, sizeof(cwd));
+			print(STDOUT_FILENO, cwd, "\n", NULL);
+		}
+		else
+		{
+			print(STDOUT_FILENO, cwd, "\n", NULL);
+			_Free(oldpwd[2]);
+			_Free(pwd[2]);
+		}
+	}
+	else
+	{
+		if (chdir(arugments[1]) == -1)
+		{
+			if (errno == ENOENT)
+			{
+				char *error = "./hsh: 1: cd: can't cd to ";
+
+				print(STDERR_FILENO, error, arugments[1], "\n", NULL);
+			}
+			else if (errno == EACCES)
+			{
+				char *error = ": Permission denied\n";
+
+				print(STDERR_FILENO, "./hsh: line 1: cd: ", arugments[1], error, NULL);
+			}
+			arguments_free(pwd);
+			arguments_free(oldpwd);
+			arguments_free(arugments);
+			return (0);
+		}
+	}
 }
